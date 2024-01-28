d2f06528a51711ba3e963ce93ea512dd6da28f1d
pzur <paolo.minaya.c@gmail.com>
2024-01-28 03:05:07 -0500

    get API

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import {API_KEY} from "./secrets.js";
 console.log('hello people');
+
+async function getTrendingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    const movies = data.results;
+    console.log({data , movies});
+
+    movies.forEach(movie => {
+        
+    });
+}
+
+getTrendingMoviesPreview();
--- src/secrets.examples.js
+++ src/secrets.examples.js
@@ -1,3 +1,5 @@
 //Crea en tu archivo secrets.js con tu API KEY
 
-const API_KEY= '';
+const API_KEY ='seses';
+
+export {API_KEY};
